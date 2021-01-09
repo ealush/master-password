f19e6ba2283c96328877778f2541527a967e6482
ealush <code@ealush.com>
2021-01-09 04:07:17 +0200

    Copy with Enter

--- index.js
+++ index.js
@@ -1,113 +1,129 @@
-const $form = document.querySelector('form.master-strings');
-const $inputLength =$form.querySelector('form input.length');
-const $btnAdd = $form.querySelector('button.add');
-const $strContainer = $form.querySelector('.str-container');
-const $inputResult = document.querySelector('input.result');
+const $form = document.querySelector("form.master-strings");
+const $inputLength = $form.querySelector("form input.length");
+const $btnAdd = $form.querySelector("button.add");
+const $strContainer = $form.querySelector(".str-container");
+const $inputResult = document.querySelector(".result input");
 
 const CLEAR_OUTPUT_TIMEOUT_MS = 1000 * 30;
 let clearOutputTimeout;
 
 const newInput = () => {
-    const label = document.createElement('label');
-    label.classList.add('str');
-    label.innerHTML = '<input type="text"/><button class="delete">&times;</button>';
-    return label;
+  const label = document.createElement("label");
+  label.classList.add("str");
+  label.innerHTML =
+    '<input type="text"/><button class="delete">&times;</button>';
+  return label;
 };
 
 const addInput = () => {
-    const $inputWrapper = newInput();
-    $strContainer.appendChild($inputWrapper);
-    $inputWrapper.querySelector('input').focus();
+  const $inputWrapper = newInput();
+  $strContainer.appendChild($inputWrapper);
+  $inputWrapper.querySelector("input").focus();
 };
 
 const setClearResult = (immediate = false) => {
-    clearTimeout(clearOutputTimeout);
+  clearTimeout(clearOutputTimeout);
 
-    if (immediate) {
-        $inputResult.value = '';
-        return;
-    }
+  if (immediate) {
+    $inputResult.value = "";
+    return;
+  }
 
-    clearOutputTimeout = setTimeout(() => {
-        $inputResult.value = '';
-        Array.from(document.querySelectorAll('label.str'))
-            .forEach((n) => n.remove())
-    }, CLEAR_OUTPUT_TIMEOUT_MS);
+  clearOutputTimeout = setTimeout(() => {
+    $inputResult.value = "";
+    Array.from(document.querySelectorAll("label.str")).forEach((n) =>
+      n.remove()
+    );
+  }, CLEAR_OUTPUT_TIMEOUT_MS);
 };
 
 addInput();
 
-vent($btnAdd).on('click', addInput);
+vent($btnAdd).on("click", addInput);
 
 const removeLabel = (target) => {
-    let next = target.parentElement.previousElementSibling
-        || target.parentElement.nextElementSibling;
-    target.parentElement.remove();
+  let next =
+    target.parentElement.previousElementSibling ||
+    target.parentElement.nextElementSibling;
+  target.parentElement.remove();
 
-    if (next) {
-        next.focus();
-    }
+  if (next) {
+    next.focus();
+  }
 
-    setClearResult(/*immediate:*/true);
+  setClearResult(/*immediate:*/ true);
 };
 
 const onArrowDown = (target) => {
-    if (target.parentElement.nextElementSibling) {
-        target.parentElement.nextElementSibling.focus();
-        return;
-    }
+  if (target.parentElement.nextElementSibling) {
+    target.parentElement.nextElementSibling.focus();
+    return;
+  }
 
-    if (target.value) {
-        addInput();
-    }
+  if (target.value) {
+    addInput();
+  }
 };
 
 const onArrowUp = (target) => {
-    if (target.parentElement.previousElementSibling) {
-        target.parentElement.previousElementSibling.focus();
-        return;
-    }
+  if (target.parentElement.previousElementSibling) {
+    target.parentElement.previousElementSibling.focus();
+    return;
+  }
 
-    $inputLength.focus();
+  $inputLength.focus();
 };
 
 vent($form)
-    .on('click', '.delete', ({ target }) => removeLabel(target))
-    .on('keyup', '.str input', (e) => {
-        if ([e.key, e.code].includes('Escape')) {
-            removeLabel(e.target);
-        } else if ([e.key, e.code].includes('ArrowDown')) {
-            onArrowDown(e.target);
-        } else if ([e.key, e.code].includes('ArrowUp')) {
-            onArrowUp(e.target);
-        }
-    })
-    .on('submit', (e) => {
-        e.preventDefault();
+  .on("click", ".delete", ({ target }) => removeLabel(target))
+  .on("keyup", ".str input", (e) => {
+    if ([e.key, e.code].includes("Escape")) {
+      removeLabel(e.target);
+    } else if ([e.key, e.code].includes("ArrowDown")) {
+      onArrowDown(e.target);
+    } else if ([e.key, e.code].includes("ArrowUp")) {
+      onArrowUp(e.target);
+    }
+  })
+  .on("submit", (e) => {
+    e.preventDefault();
 
-        const values = [...$form.querySelectorAll('input')].map(({ value }) => value);
+    const values = [...$form.querySelectorAll("input")].map(
+      ({ value }) => value
+    );
 
-        sendMessage(values);
-    });
+    vent($inputResult).trigger("focus");
 
-vent($inputResult).on('click', ({ target }) => {
-    target.select();
-    document.execCommand('copy');
-    setClearResult();
+    sendMessage(values);
+  });
+
+vent($inputResult).on("click", ({ target }) => {
+  copyResult(target);
+});
+
+vent($inputResult).on("keyup", (e) => {
+  if ([e.key, e.code].includes("Enter")) {
+    copyResult(e.target);
+  }
 });
 
 const sendMessage = (msg) => {
-    navigator.serviceWorker.controller.postMessage(msg);
+  navigator.serviceWorker.controller.postMessage(msg);
 };
 
 (async () => {
-    await navigator.serviceWorker.register('./sw.js', {
-        updateViaCache: 'none'
-    });
+  await navigator.serviceWorker.register("./sw.js", {
+    updateViaCache: "none",
+  });
 
-    vent(navigator.serviceWorker)
-        .on('message', ({ data }) => {
-            $inputResult.value = data;
-            setClearResult();
-        });
+  vent(navigator.serviceWorker).on("message", ({ data }) => {
+    $inputResult.value = data;
+    setClearResult();
+  });
 })();
+
+function copyResult(target) {
+  target.select();
+  document.execCommand("copy");
+  setClearResult(true);
+}
